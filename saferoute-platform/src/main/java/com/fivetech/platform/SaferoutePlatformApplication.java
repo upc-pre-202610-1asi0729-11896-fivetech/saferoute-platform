d67896630f20d6895495d0380313b897fa940c27
Frase <ytcarniy@gmail.com>
2026-06-14 01:14:00 -0500

    init repository

---
+++ saferoute-platform/src/main/java/com/fivetech/platform/SaferoutePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fivetech.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferoutePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaferoutePlatformApplication.class, args);
+    }
+
+}
---
+++ saferoute-platform/src/test/java/com/fivetech/platform/SaferoutePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivetech.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferoutePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
